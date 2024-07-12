544840e06180b0be6843552adbeac884a98bccbf
LuihGuerra <zavalafernando262@gmail.com>
2024-07-12 15:30:08 -0600

    Add files via upload

---
+++ Calculadora/src/interfaz/Interfaz.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interfaz;
+
+/**
+ *
+ * @author juanalvarenga
+ */
+public class Interfaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+
+//            ventana.dispose(); // destruye de memoria la ventana
+
+            Calculadora ventanaCalculadora = new Calculadora();
+            ventanaCalculadora.setVisible(true);
+
+    }
+
+}
